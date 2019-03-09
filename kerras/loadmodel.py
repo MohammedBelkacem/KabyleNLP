16f3cbbadf4e5a174e41d01818eb058181d4992a
MozillaKab <7033740+mozillakab@users.noreply.github.com>
2019-03-09 17:07:20 +0100

    Create loadmodel.py

---
+++ kerras/loadmodel.py
@@ -0,0 +1,8 @@
+from keras.models import Sequential
+from keras.models import load_model
+from keras.layers import Dense
+from keras.models import model_from_json
+import numpy
+import os
+#weights_path = 'd:/tal/keras_mlp.h5', 'r'
+loaded_model = load_model('d:/tal/keras_mlp.h5')
