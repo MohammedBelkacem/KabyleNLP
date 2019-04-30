1945ba456d01bc0e7cf6964b4e945ba9e133dd77
MozillaKab <7033740+mozillakab@users.noreply.github.com>
2019-04-30 21:56:45 +0200

    Update generate-kabfr-kabeng-sentences.py

--- Tatoeba-corpus/generate-kabfr-kabeng-sentences.py
+++ Tatoeba-corpus/generate-kabfr-kabeng-sentences.py
@@ -1,35 +1,28 @@
-kabfr=open("sentences_kab_fra.csv",encoding='utf-8')
-
-numbers=[]
-numbers2=[]
-for line in kabfr:
+f= open("c:/tal/tatoeba/sentences/sentences.csv",encoding='utf-8')
+kabfr=open("c:/tal/tatoeba/sentences/sentences_kab_fra.csv","w+",encoding='utf-8')
+kaben=open("c:/tal/tatoeba/sentences/sentences_kab_eng.csv","w+",encoding='utf-8')
+i=0
+line=""
+for line in f:
         line=line.replace("\ufeff","")
+        #values = line.strip()
         values = line.split("\t")
-        numbers.append(values[0])
-kabfr.close()
 
-kaben=open("sentences_kab_eng.csv",encoding='utf-8')
+        #print(values[1])
+        if (values[1]=="fra") or (values[1]=="kab"):
+         kabfr.write(line)
+        if (values[1]=="eng") or (values[1]=="kab"):
+         kaben.write(line)
+         #print(line[12:])
+         #i=i+1
 
-for line in kaben:
-        line=line.replace("\ufeff","")
-        values = line.split("\t")
-        numbers2.append(values[0])
-        
+
+##        if i>12:
+##            exit()
+
+
+
+
+f.close()
 kaben.close()
-
-
-
-links=open("links.csv",encoding='utf-8')
-linkskabfr=open("linksfr.csv","w+",encoding='utf-8')
-linkskaben=open("linksen.csv","w+",encoding='utf-8')
-for line in links:
-        line1=line.replace("\ufeff","")
-        line1=line1.strip()
-        values = line1.split("\t")
-        if values[0] in numbers or values[1] in numbers:
-             linkskabfr.write(line)
-        if values[0] in numbers2 or values[1] in numbers2:
-             linkskaben.write(line)
-links.close()
-linkskabfr.close()
-linkskaben.close()
+kabfr.close()
