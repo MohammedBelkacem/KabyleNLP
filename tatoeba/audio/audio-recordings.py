030557d292fd17ef724e912de5ad454f99a73f01
MozillaKab <7033740+mozillakab@users.noreply.github.com>
2019-08-28 11:15:07 +0200

    Update audio-recordings.py

--- tatoeba/audio/audio-recordings.py
+++ tatoeba/audio/audio-recordings.py
@@ -1,13 +1,6 @@
-#By Mohammed Belkacem 2019
-#Apache License
-#Version 2.0, January 2004
-#http://www.apache.org/licenses
-
-
 #First, you have to download the sentence file and the audio file. The sentence file contains all sentences, the audio
 #file contains Ids of the recorded sentences.
-# extract lang (a 3 char code) sentences from src file and generate target file. Example for english lang="eng", 
-#kabyle: lang="kab"
+# extract lang sentences from src file and generate target file
 
 lang="kab"
 src_file="sentences.csv"
@@ -48,38 +41,44 @@ def extract_sentences_ids(text_file,sentences_ids):
   return sentences_ids
 
 # create a text file for sentences wich have only recordings
+sentences_ids= extract_sentences_ids(text_file,sentences_ids)
+print (sentences_ids[:10])
 target_audio_file= "sentences_kab_with_audio.csv"
 audio_file="sentences_with_audio.csv"
 
-sentences_ids= extract_sentences_ids(text_file,sentences_ids)
-print (sentences_ids[:10])
 
-def generate_lang_file_audio (target_audio_file,audio_file,sentences_ids):
+
+def generate_lang_file_audio (target_audio_file,audio_file,sentences_ids,lang):
 
     kab_audio=open(target_audio_file,"w+",encoding='utf-8')
     f= open(audio_file,encoding='utf-8')
 
     for line in f:
             line=line.replace("\ufeff","")
+
             values = line.split("\t")
+
             if int(values[0]) in sentences_ids:
              #audiokab.append(values[1])
-             kab_audio.write(values[0]+"\t"+values[3]+"\n")
+             kab_audio.write("https://audio.tatoeba.org/sentences/"+lang+"/"+values[0]+".mp3\n")
     kab_audio.close()
     f.close()
 
-generate_lang_file_audio (target_audio_file,audio_file,sentences_ids)
+generate_lang_file_audio (target_audio_file,audio_file,sentences_ids,lang)
 
 # download the audio regordings
 import wget
 
-src_file="sentences_with_audio.csv"
+target_audio_file="sentences_with_audio.csv"
 
-def download (src_file):
-    f=open(src_file,encoding='utf-8')
+def download (target_audio_file):
+    f=open(target_audio_file,encoding='utf-8')
     for url in f:
         values = line.split("\t")
-        wget.download(values[1])
-    f.close()
-download (src_file)
+        try:
+         wget.download(values[1])
+        except:
+         print ("file error", values[1])
 
+    f.close()
+download (target_audio_file)
